aa6a6834e77744257ce913edf9f0b14aa591bbe1
Iuliia Bondarenko <bondarienko-iuliia@mail.ru>
2022-04-21 13:48:26 +0300

    ui changes

--- Paper Rock and Scissors/ContentView.swift
+++ Paper Rock and Scissors/ContentView.swift
@@ -11,153 +11,169 @@ extension Color{
     static let darkerGray = Color(red: 0.92, green: 0.92, blue: 0.93)
 }
 
-struct NeumorphicButtonStyle: ButtonStyle{
-    func makeBody(configuration: Self.Configuration)-> some View {
-        configuration.label
-            .padding(25)
-            .background(
-                Group{
-                    if(configuration.isPressed){
-                    Circle()
-                        .fill(Color.lightGray)
-                      
-                }
-                else{
-                    Circle()
-                        .fill(Color.lightGray)
-                        .shadow(color: Color.black.opacity(0.15), radius: 10, x: 10, y: 10)
-                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
-
+    struct NeumorphicButtonStyle: ButtonStyle{
+        func makeBody(configuration: Self.Configuration)-> some View {
+            configuration.label
+                .padding(25)
+                .background(
+                    Group{
+                        if(configuration.isPressed){
+                            Circle()
+                                .fill(Color.lightGray)
+                                .overlay{
+                                    Circle()
+                                        .stroke(Color.gray, lineWidth: 4)
+                                        .blur(radius: 4)
+                                        .offset(x: 2, y: 2)
+                                        .mask(Circle().fill(LinearGradient(gradient: Gradient(colors : [Color.clear, Color.black]), startPoint: .topLeading, endPoint: .bottomLeading)))
+                                        .overlay{
+                                            Circle()
+                                                .stroke(Color.white, lineWidth: 8)
+                                                .blur(radius: 4)
+                                                .offset(x: -2, y: -2)
+                                                .mask(Circle().fill(LinearGradient(gradient: Gradient(colors : [Color.clear, Color.black]), startPoint: .topLeading, endPoint: .bottomLeading)))
+                                            
+                                        }}
+                        }
+                        else{
+                            Circle()
+                                .fill(Color.lightGray)
+                                .shadow(color: Color.black.opacity(0.15), radius: 10, x: 10, y: 10)
+                                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                            
+                        }
+                        
                     }
-                    
-                }
-            )
+                )
+        }
     }
     
-    
-}
-
-struct ContentView: View {
-    @State var gameMove = moves.paper
-    @State var isWin = true
-    @State var score = 0
-    @State var showAlert = false
-    enum moves: String, CaseIterable{
-        case paper = "paper"
-        case scissors = "scissors"
-        case rock = "rock"
-    }
-    
-    init(){
-        newMove()
-    }
-    let winPairs = [moves.paper : moves.scissors, moves.rock : moves.paper, moves.scissors : moves.rock]
-    func restartGame(){
-        score = 0
-        newMove()
-    }
-    func newMove(){
-      gameMove = moves.allCases.randomElement()!
-       isWin = Bool.random()
-    }
-    
-//user won when
-    func isPlayerWon (playerMove:moves)-> Bool{
+    struct ContentView: View {
+        @State var gameMove = moves.paper
+        @State var isWin = true
+        @State var score = 0
+        @State var showAlert = false
+        enum moves: String, CaseIterable{
+            case paper = "paper"
+            case scissors = "scissors"
+            case rock = "rock"
+        }
         
-        if let playeShouldChooseToWin = winPairs[gameMove], playeShouldChooseToWin == playerMove , isWin{
-          return true
+        init(){
+            newMove()
         }
-        else if let playerSouldChooseToLoose = winPairs[playerMove], playerSouldChooseToLoose == gameMove {
-            return true
+        let winPairs = [moves.paper : moves.scissors, moves.rock : moves.paper, moves.scissors : moves.rock]
+        func restartGame(){
+            score = 0
+            newMove()
         }
-        return false
-    }
-    func calculateScore(playerMove:moves){
-        if isPlayerWon(playerMove: playerMove){
-            score += 1
-            if score == 10{
-                restartGame()
-                showAlert = true
+        func newMove(){
+            gameMove = moves.allCases.randomElement()!
+            isWin = Bool.random()
+        }
+        
+        //user won when
+        func isPlayerWon (playerMove:moves)-> Bool{
+            
+            if let playeShouldChooseToWin = winPairs[gameMove], playeShouldChooseToWin == playerMove , isWin{
+                return true
             }
+            else if let playerSouldChooseToLoose = winPairs[playerMove], playerSouldChooseToLoose == gameMove {
+                return true
+            }
+            return false
         }
-
-    }
-    var body: some View {
-        ZStack{
-            Color.lightGray
-                .ignoresSafeArea()
-        VStack{
-        Text("Score: \(score)")
-            Spacer()
-            VStack{
-            Image(systemName: String(gameMove == moves.paper ? "doc.fill": gameMove == moves.rock ? "oval.fill" : "scissors")).font(.largeTitle)
-            Text(isWin ? "Win" : "Loose")
-            }.frame(maxWidth: .infinity, maxHeight: 200)
-                .background(Color.lightGray)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-            
-                .cornerRadius(20)
-                .overlay{
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.gray, lineWidth: 4)
-                        .blur(radius: 4)
-                        .offset(x: 2, y: 2)
-                        .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors : [Color.clear, Color.black]), startPoint: .topLeading, endPoint: .bottomLeading)))
-                        .overlay{
-                                           RoundedRectangle(cornerRadius: 20)
-                                               .stroke(Color.white, lineWidth: 8)
-                                               .blur(radius: 4)
-                                               .offset(x: -2, y: -2)
-                                               .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors : [Color.clear, Color.black]), startPoint: .topLeading, endPoint: .bottomLeading)))
-
-
-                                       }
-                                   
-
+        func calculateScore(playerMove:moves){
+            if isPlayerWon(playerMove: playerMove){
+                score += 1
+                if score == 10{
+                    restartGame()
+                    showAlert = true
                 }
-               
+            }
             
-            Spacer()
-          
-            HStack(spacing: 35){
-        
-                Button {
-                    calculateScore(playerMove:  moves.rock)
-                    newMove()
-                }label:{
-                    Image(systemName: "doc.fill")
-                    }.buttonStyle(NeumorphicButtonStyle())
-                    
-                Button{
-                    calculateScore(playerMove:  moves.rock)
-                    newMove()
-                }label: {
-                    Image(systemName: "oval.fill")
-                }.buttonStyle(NeumorphicButtonStyle())
-                Button{
-                    calculateScore(playerMove:  moves.scissors)
-                    newMove()
-                }label: {
-                    Image(systemName: "scissors")
-                }.buttonStyle(NeumorphicButtonStyle())
-                
-            }.padding()
-        }.padding()
         }
-        
+        var body: some View {
+            ZStack{
+                Color.lightGray
+                    .ignoresSafeArea()
+                VStack{
+                    HStack{
+                        Text("Score: \(score)").padding(.horizontal, 5)
+                        Spacer()}
+                    
+                    Spacer()
+                    VStack{
+                        Image(systemName: String(gameMove == moves.paper ? "doc.fill": gameMove == moves.rock ? "oval.fill" : "scissors")).font(.largeTitle)
+                            .padding(10)
+                        Text(isWin ? "Win" : "Loose")
+                    }.frame(maxWidth: .infinity, maxHeight: 200)
+                        .background(Color.darkerGray)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                     
+                        .cornerRadius(20)
+                        .overlay{
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                                .blur(radius: 3)
+                                .offset(x: 2, y: 2)
+                                
+                                .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors : [Color.clear, Color.black]), startPoint: .topLeading, endPoint: .bottomLeading)))
+                                .overlay{
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.white, lineWidth: 7)
+                                        .blur(radius: 3)
+                                        .offset(x: -2, y: -2)
+                                        .mask(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors : [Color.clear, Color.black]), startPoint: .topLeading, endPoint: .bottomLeading)))
+                                    
+                                    
+                                }
+                            
+                            
+                        }
+                    
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 35){
+                        
+                        Button {
+                            calculateScore(playerMove:  moves.rock)
+                            newMove()
+                        }label:{
+                            Image(systemName: "doc.fill")
+                        }.buttonStyle(NeumorphicButtonStyle())
+                        
+                        Button{
+                            calculateScore(playerMove:  moves.rock)
+                            newMove()
+                        }label: {
+                            Image(systemName: "oval.fill")
+                        }.buttonStyle(NeumorphicButtonStyle())
+                        Button{
+                            calculateScore(playerMove:  moves.scissors)
+                            newMove()
+                        }label: {
+                            Image(systemName: "scissors")
+                        }.buttonStyle(NeumorphicButtonStyle())
+                        
+                    }.padding()
+                }.padding()
+            }
+            
+        }
     }
-}
-
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
-// on screen the card is flipping with image and text
-// 3 buttons become  active
-// alert about win or loose
-// score is in the corner
-// end te game with screen showing score
+    // on screen the card is flipping with image and text
+    // 3 buttons become  active
+    // alert about win or loose
+    // score is in the corner
+    // end te game with screen showing score
